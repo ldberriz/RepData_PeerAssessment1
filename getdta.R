61562103a1419d095481a6b8e4d8d32cee2e9357
ldberriz <ldberriz@ctsx.net>
2015-01-17 09:36:07 -0500

    Minor Revs

--- getdta.R
+++
@@ -1,7 +0,0 @@
-getdta <- function() {
-  cacheFile <- "activity.csv"
-  dta <- read.csv(cacheFile)
-  dta$date <- strptime(dta$date, "%Y-%m-%d")  ## Convert the date to POSIX
-  dta
-}
-
--- test02.R
+++ test02.R
@@ -14,3 +14,5 @@ fill.data <- function() {
     }
   dta.filled  # Return object
   }
+
+
