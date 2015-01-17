3a90d9314d63098f8b598c749ab08d4ae83bf012
ldberriz <ldberriz@ctsx.net>
2015-01-17 00:05:03 -0500

    Added graph

--- test01.R
+++ test01.R
@@ -1,11 +1,20 @@
-cnt = 0
+
 for (i in (1:nrow(pp))) {
-  if (is.na(pp[i,c("steps")])) {
-    t1 <- pp[i,c("interval")]
-    t2 <- steps.interval[(steps.interval$interval==t1),]
+  if (is.na(dta[i,"steps"])) {
+    t1 <- dta[i,"interval"]
+    t2 <- steps.interval[(steps.interval$interval==t1),"steps"]
+    dta.pre <- pp[i,"steps"]
+    
+    dta[i,"steps"] <- t2
+    dta.post <- pp[i,"steps"]
+    ###########
     cnt = cnt + 1
-    print(paste("index=",t1, "pp$interval=",t1,"steps.interval$interval=",t2))
+    print(paste("pp$interval=",t1,"steps.interval$interval=",t2, pp.dta.pre, pp.dta.post))
   }
 }
-print(paste("cnt =",cnt))
 
+
+
+steps.by.day.new <- aggregate(steps ~ date, data=df.new, FUN=sum)
+ggplot(steps.by.day.new, aes(x=steps)) +
+  geom_histogram(binwidth=2500, colour="black", fill="white")
