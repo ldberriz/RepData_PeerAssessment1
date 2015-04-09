8bcc84a6ef9b183da03b938ed0164136fcf28538
ldberriz <ldberriz@kennis-research.com>
2015-04-09 16:04:58 -0400

    Update April 2015

--- test02.R
+++ test02.R
@@ -1,5 +1,5 @@
 # Function to generate a data frame where the NA values have been
-# replaced by the mean of the values fo rthat interval
+# replaced by the mean of the values for that interval
 
 fill.data <- function() {
   dta.filled <- dta  # Create a data frame dta.filled to save the data with the missing values filled.
@@ -16,3 +16,14 @@ fill.data <- function() {
   }
 
 
+dta.filled <- dta
+fill.data2 <- function() {
+  if (is.na(dta.filled$steps)) {   # Check the value for steps is missing
+    t1 <- dta.filled$interval      # the value of that interval in t1.  Should be between 0 and 2355
+    # Use the index t2 to map into the correct row of the steps.interval data frame that 
+    # has the value of the means for each interval.
+    t2 <- steps.interval[(steps.interval$interval==t1),"steps"]  
+    dta.filled[i,"steps"] <- t2         # Fill the missing data.
+    # print(paste(i, dta.filled[i,"steps"]))  # Debug code
+  }  
+}
